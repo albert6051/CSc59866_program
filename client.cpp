fa20ba51dd1d455b670482ff2ad4878ce5d6d1a9
albert6051 <albert.chen6051@gmail.com>
2019-05-17 14:59:00 -0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -15,8 +15,6 @@
 
 using namespace std;
 
-#define PORT "80" // the port client will be connecting to 
-
 #define MAXDATASIZE 4096 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
@@ -31,6 +29,8 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char *argv[])
 {
+
+    char *PORT = argv[2];
     // declare all the variable and the buffet that is needed
     int sockfd, len, bytes_sent, rv, fd;  
     char buf[MAXDATASIZE], imgBuf[MAXDATASIZE];
@@ -42,8 +42,8 @@ int main(int argc, char *argv[])
     FILE *fptr, *fpti;
 
     // check if the input has the correct amount of parameter
-    if (argc != 2 && argc != 3) {
-        fprintf(stderr,"usage: client hostname\n");
+    if (argc != 3 && argc != 4 && argc != 5) {
+        fprintf(stderr,"Please enter the proper numbers of argument\n");
         exit(1);
     }
 
@@ -57,8 +57,6 @@ int main(int argc, char *argv[])
     slice_point = fullUrl.find("/");;
     string hostname = fullUrl.substr(0, slice_point);
     string directory = fullUrl.substr(slice_point);
-    
-    printf("%s, %s \n",hostname.c_str(), directory.c_str());
 
     // get the address information for the host
     if ((rv = getaddrinfo(hostname.c_str(), PORT, &hints, &servinfo)) != 0) {
@@ -98,22 +96,43 @@ int main(int argc, char *argv[])
     bzero(msg, 1000);
 
     // check if there is command lin options, -t or -h, and generate the HTTP request accordingly
-    if (argc == 2)
+    if (argc == 3)
     {
         sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", directory.c_str(), hostname.c_str());
-    }else if (argv[2][1] == 'h')
+    }else if (argv[3][1] == 'h')
     {
         sprintf(msg, "HEAD %s HTTP/1.1\r\nHost: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", directory.c_str(), hostname.c_str());
-    }else if (argv[2][1] == 't')
+    }else if (argv[3][1] == 't')
     {
-        sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nIf-Modified-Since: Wed, 10 Apr 2019 13:42:00 GMT\r\nIf-None-Match: \"122c8-58631a92bfc30\"\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", directory.c_str(), hostname.c_str());
+        if(argc != 5){
+            fprintf(stderr,"The numbers of arguments is not match\n");
+            exit(1);
+        }
+        sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nIf-Modified-Since: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", directory.c_str(), hostname.c_str(), argv[4]);
     }
     // print the HTTP request
     printf("\n%s", msg);
     // sent HTTP request to the server
     bytes_sent = send(sockfd, msg, strlen(msg), 0);
 
-    if (argc == 2 || argv[2][1] == 't') {
+    if (argc == 3 || argv[3][1] == 't') {
+        size_t numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
+        if ( numbytes == -1 ) perror("recv");
+        string holder = buf;
+        size_t headerFound = holder.find("HTTP/1.1");
+        // try to split the header information from the all the data send by server
+        string header;
+        string content;
+        if(headerFound == 0){ // header will appear in the beginning of the message
+            size_t endFound = holder.find("\r\n\r\n");
+            header = holder.substr(0, endFound+4);
+            content = holder.substr(endFound+4);
+            printf("%s", header.c_str());
+            string check = header.substr(0, 12);
+            if(check != "HTTP/1.1 200"){
+                exit(1);
+            }
+        }
         // find the position split the folder path and file name
         size_t found = directory.rfind('/');
         // split the directory into the folder path and file name
@@ -140,26 +159,16 @@ int main(int argc, char *argv[])
                 printf("\n open() failed with error [%s}\n", strerror(errno));
             }else
             {   
+                if ( fwrite(content.c_str(), 1, strlen(content.c_str()), fptr) == -1 ) perror("file write failed");
+                total += strlen(content.c_str());
                 // continue to fetch the data from server until everything is download
                 while (1)
                 {
-                    size_t numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
+                    numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
                     if ( numbytes == -1 ) perror("recv");
                     if ( numbytes == 0 ) break; // got to the end of the stream
-                    string holder = buf;
-                    size_t headerFound = holder.find("HTTP/1.1");
-                    // try to split the header information from the all the data send by server
-                    if(headerFound == 0){ // header will appear in the beginning of the message
-                        size_t endFound = holder.find("\r\n\r\n");
-                        string header = holder.substr(0, endFound+4);
-                        string content = holder.substr(endFound+4);
-                        printf("%s", header.c_str());
-                        if ( fwrite(content.c_str(), 1, strlen(content.c_str()), fptr) == -1 ) perror("file write failed");
-                        total += strlen(content.c_str());
-                    }else{
-                        if ( fwrite(buf, sizeof(char), numbytes, fptr) == -1 ) perror("file write failed");
-                        total += numbytes;
-                    }
+                    if ( fwrite(buf, sizeof(char), numbytes, fptr) == -1 ) perror("file write failed");
+                    total += numbytes;
                 }
                 // connection is lost therefore reconnect
                 for(p = servinfo; p != NULL; p = p->ai_next) {
@@ -183,24 +192,49 @@ int main(int argc, char *argv[])
                 rewind(fptr);
                 fread(temp, sizeof(char), size, fptr);
                 string search = temp;
-                size_t imgFound = search.find("<img");
-                // check if any image is found
-                if(imgFound != string::npos){
-                    printf("%d %d\n", imgFound, string::npos);
+                size_t srcFound = search.find("src=");
+                size_t preFound = srcFound;
+                // check if any other object is found
+                while(1){
+                    preFound = srcFound;
                     // find the file directory and truncate it
-                    search = search.substr(imgFound);
-                    size_t srcFound = search.find("src=");
-                    string imgLoc = search.substr(srcFound);
-                    size_t firstQuote = imgLoc.find('\"');
-                    imgLoc = imgLoc.substr(firstQuote + 1);
-                    size_t secondQuote = imgLoc.find('\"');
-                    imgLoc = imgLoc.substr(0, secondQuote);
-                    string url = "/" + path + imgLoc;
+                    string srcLoc = search.substr(srcFound);
+                    size_t firstQuote = srcLoc.find('\"');
+                    srcLoc = srcLoc.substr(firstQuote + 1);
+                    size_t secondQuote = srcLoc.find('\"');
+                    srcLoc = srcLoc.substr(0, secondQuote);
+                    string url = "/" + path + srcLoc;
                     // create request for image
                     bzero(msg, 1000);
-                    sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", url.c_str(), hostname.c_str());
+                    if(argc == 5){
+                        sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nIf-Modified-Since: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", url.c_str(), hostname.c_str(), argv[4]);
+                    }else{
+                        sprintf(msg, "GET %s HTTP/1.1\r\nHost: %s\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n\r\n", url.c_str(), hostname.c_str());
+                    }
                     bytes_sent = send(sockfd, msg, strlen(msg), 0);
                     printf("%s", msg);
+
+                    size_t numbytes = recv(sockfd, imgBuf, MAXDATASIZE-1, 0);
+                    if ( numbytes == -1 ) perror("recv");
+                    string holder = imgBuf;
+                    size_t headerFound = holder.find("HTTP/1.1");
+                    // try to split the header information from the all the data send by server
+                    char header[300];
+                    size_t headerLength;
+                    size_t contentLength;
+                    if(headerFound == 0){ // header will appear in the beginning of the message
+                        size_t endFound = holder.find("\r\n\r\n");
+                        headerLength = endFound+4;
+                        contentLength = numbytes-headerLength;
+                        strncpy( header, imgBuf, headerLength );
+                        header[headerLength]='\0';
+                        string headerCK = header;
+                        printf("%s", header);
+                        string check = headerCK.substr(0, 12);
+                        if(check != "HTTP/1.1 200"){
+                            exit(1);
+                        }
+                    }
                     // find the position split the folder path and file name
                     size_t found = url.rfind('/');
                     // split the directory into the folder path and file name
@@ -213,42 +247,34 @@ int main(int argc, char *argv[])
                     // use the command line to make system call to create folders and file
                     const int dir_err = system(pathFileGen);
                     fpti = fopen( fileName.c_str(), "wb+");
+                    if ( fwrite(imgBuf+headerLength, 1, contentLength, fpti) == -1 ) perror("file write failed");
+                    total += numbytes;
+
                     while (1)
-                    {
+                    {   
                         size_t numbytes = recv(sockfd, imgBuf, MAXDATASIZE-1, 0);
                         if ( numbytes == -1 ) perror("recv");
-                        printf("%d", numbytes);
                         if ( numbytes == 0 ) break; // got to the end of the stream
-                        string holder = imgBuf;
-                        size_t headerFound = holder.find("HTTP/1.1");
-                        // try to split the header information from the all the data send by server
-                        if(headerFound == 0){ // header will appear in the beginning of the message
-                            size_t endFound = holder.find("\r\n\r\n");
-                            string header = holder.substr(0, endFound+4);
-                            string content = holder.substr(endFound+4);
-                            printf("%s", header.c_str());
-                            if ( fwrite(content.c_str(), 1, strlen(content.c_str()), fpti) == -1 ) perror("file write failed");
-                            total += strlen(content.c_str());
-                        }else{
-                            if ( fwrite(imgBuf, sizeof(char), numbytes, fpti) == -1 ) perror("file write failed");
-                            total += numbytes;
-                        }
+                        if ( fwrite(imgBuf, sizeof(char), numbytes, fpti) == -1 ) perror("file write failed");
+                        total += numbytes;
                     }
-                    delete[] temp;
                     fclose(fpti);
-                    imgFound = search.find("<img");
+                    srcFound = search.find("src=");
+                    if(srcFound == preFound){
+                        break;
+                    }
                 }
+                delete[] temp;
                 fclose(fptr);
             }
         }
-    }else if(argv[2][1] == 'h')
+    }else if(argv[3][1] == 'h')
     {
         // get the head infromation from server and print it
         size_t numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0);
         buf[numbytes] = '\0';
         printf("%s", buf);
     }
-    
     freeaddrinfo(servinfo); // all done with this structure
     // close the socket
     close(sockfd);
